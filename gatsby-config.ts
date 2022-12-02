2e4b5940f2128f772a9e9f598d71585971fe86dd
Evan Gray <battledingo@gmail.com>
2022-12-01 21:40:43 -0500

    github actions

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -10,6 +10,7 @@ const siteMetadata = {
 };
 
 const config: GatsbyConfig = {
+  pathPrefix: process.env.GATSBY_PATH_PREFIX || undefined,
   siteMetadata,
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
